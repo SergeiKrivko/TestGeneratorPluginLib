6ba4d20d8a01b17f0fb961322e77e1c9596d4fbe
SergeiKrivko <sergik.krivko@gmail.com>
2024-05-16 16:30:11 +0300

    Фикс багов

--- TestGeneratorPluginLib/__init__.py
+++ TestGeneratorPluginLib/__init__.py
@@ -1,6 +1,10 @@
-from TestGeneratorPluginLib._widgets import MainTab, SideTab, SideTabButton
-from TestGeneratorPluginLib._managers import BackendManager, Manager
-from TestGeneratorPluginLib._language import Language, FastRunFunction, FastRunCommand
+try:
+    from TestGeneratorPluginLib._widgets import MainTab, SideTab, SideTabButton
+    from TestGeneratorPluginLib._managers import BackendManager, Manager
+    from TestGeneratorPluginLib._language import Language, FastRunFunction, FastRunCommand
 
-from TestGeneratorPluginLib._plugin import Plugin
+    from TestGeneratorPluginLib._plugin import Plugin
+except Exception:
+    pass
+
 from TestGeneratorPluginLib._plugin_setup import PluginSetup
